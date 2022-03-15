b2594a9afb56bd0dd17fe6c44a69352b016f9120
Jose Luis Lopez Lopez Prieto <ing.jlllopezp@gmail.com>
2022-03-14 23:04:50 -0600

    Added: app.py and products.py

---
+++ products.py
@@ -0,0 +1,10 @@
+"""
+Esto hará la función de una base de datos, este ejercicio se puede mejorar haciéndole su
+base de datos en prostgreSQL
+"""
+
+products = [
+    {"name": "laptop", "price": 800, "quantity": 4},
+    {"name": "mouse", "price": 40, "quantity": 10},
+    {"name": "monitor", "price": 400, "quantity": 3}
+]
